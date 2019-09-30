ea7e64f8901b2b1b0f3cbc7adee77d4161a7e692
eveporcello <eve@moonhighway.com>
2019-09-30 10:20:18 -0700

    Adds finished files

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, MockList } = require("apollo-server");
+const faker = require("faker");
 
 const typeDefs = `
+    type Cat {
+        id: ID!
+        name: String!
+        age: Int!
+        nice: Boolean
+    }
+    type Horse {
+        id: ID!
+        name: String!
+        age: Int!
+        description: String
+    }
     type Query {
-        hello: String
+        allCats: [Cat!]!
+        allHorses: [Horse!]!
     }
 `;
 
+const horseDescriptions = ["majestic", "honorable", "street smart"];
+
+const mocks = {
+  Query: () => ({
+    allCats: () => new MockList([1, 20])
+  }),
+  Horse: () => ({
+    description: () => faker.random.arrayElement(horseDescriptions)
+  }),
+  ID: () => faker.random.uuid(),
+  Int: () => faker.random.number({ min: 1, max: 25 }),
+  String: () => faker.name.firstName(),
+  Boolean: () => faker.random.boolean()
+};
+
 const resolvers = {
   Query: {
-    hello: () => "hello world!"
+    allCats: () => [
+      {
+        id: 1,
+        name: "Meatball"
+      }
+    ]
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  mocks,
+  mockEntireSchema: false
 });
 
-server.listen().then(({ url }) => console.log(`Server Running on port ${url}`));
+server.listen().then(({ url }) => console.log(`Server running on port ${url}`));
